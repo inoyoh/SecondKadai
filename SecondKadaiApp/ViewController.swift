154b5c9e2f68bad4964d003d923d6e68191700f3
yohei inokuchi <inoyoh@gmail.com>
2020-12-06 00:57:55 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by yohei on 2020/12/06.
+//  Copyright © 2020 yohei.inokuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
